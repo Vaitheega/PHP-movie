373c0f44bbf6323b9d40a6908ec59f6d92de7de4
Vaitheega <vaitheegapria1@gmail.com>
2026-09-17 13:49:10 +0200

    Update insertdata.php

--- insertdata.php
+++ insertdata.php
@@ -11,13 +11,15 @@ if (!$link) {
     die("Connection failed: " . mysqli_connect_error());
 }
 
-$mname = $_POST['mname'];
+$mname = $_POST['mname'];    //take the movie name from the form and store it in $mname.
 $myear = $_POST['myear'];
 $mgenreid = $_POST['mgenreid'];
 $mrating = $_POST['mrating'];
 
 $sql = "INSERT INTO movies (mname, myear, mgenreid, mrating)
-        VALUES (?, ?, ?, ?)";
+        VALUES (?, ?, ?, ?)";            //This is an SQL INSERT query.
+        // We use it when we want to add a new movie to the database. 
+        // placeholders. When the user enters a new details will be put into these places later.
 
 $stmt = mysqli_prepare($link, $sql);
 
@@ -28,6 +30,6 @@ mysqli_stmt_execute($stmt);
 echo "Movie added successfully.";
 
 mysqli_stmt_close($stmt);
-mysqli_close($link);
+mysqli_close($link);        // finished working , dont need connection anymore
 
 ?>
